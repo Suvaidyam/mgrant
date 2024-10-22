dbace50bab6d191ffbcb39b5db54e15ff80a22f5
Samar Raj <samar.suvaidyam@gmail.com>
2024-10-22 08:03:05 +0000

    datatable enhancements

--- mgrant/mgrant___organisation/doctype/organisation/organisation.js
+++ mgrant/mgrant___organisation/doctype/organisation/organisation.js
@@ -2,8 +2,8 @@
 // For license information, please see license.txt
 
 
-const getDocList = (doctype, filters, fields=['*'])=>{
-    return new Promise((resolve, reject)=>{
+const getDocList = (doctype, filters, fields = ['*']) => {
+    return new Promise((resolve, reject) => {
         frappe.call({
             method: "frappe.client.get_list",
             args: {
@@ -12,10 +12,10 @@ const getDocList = (doctype, filters, fields=['*'])=>{
                 fields,
                 order_by: 'creation desc'
             },
-            callback: function(response) {
+            callback: function (response) {
                 resolve(response.message);
             },
-            error:(err)=>{
+            error: (err) => {
                 reject(err)
             }
         });
@@ -26,8 +26,8 @@ const getViewSettings = (doctype) => {
     return new Promise((resolve, reject) => {
         frappe.call({
             method: "frappe_theme.api.get_my_list_settings",
-            args: { doctype: doctype},
-            callback: function(response) {
+            args: { doctype: doctype },
+            callback: function (response) {
                 if (response.message) {
                     resolve(response.message);
                 } else {
@@ -42,50 +42,34 @@ const getViewSettings = (doctype) => {
     });
 }
 
-const tabContent = async(frm, tab_field)=>{
-    let field = frm.meta?.fields?.find(f=> f.fieldname == tab_field)
-    let _fields = frm.meta?.fields?.filter(f=> field?.default?.split(',')?.includes(f.fieldname))
-    for(let _f of _fields){
-        let link = frm.meta.links?.find(f=> f.link_doctype == _f.default)
-        if(link){
-            let settings = await getViewSettings(link.link_doctype);
-            if(settings?.fields){
-                let fields = JSON.parse(settings.fields)?.map(e=> e.fieldname);;
-                let columns = await frappe.call('frappe_theme.api.get_meta_fields', { doctype: link.link_doctype });
-                let _columns = [{
-                    fieldname:'name',
-                    label:'ID'
-                },...columns?.message?.filter(f=> fields.includes(f.fieldname))]
-                
-                let rows = await getDocList(link.link_doctype,[
-                    [link.link_doctype, link.link_fieldname,'=', frm.doc.name]
-                ], ['*'])
-
-                let datatable = new SvaDataTable({
-                    wrapper:document.querySelector(`#${_f.fieldname}`), // Wrapper element
-                    columns: _columns,   // Define columns
-                    rows: rows,      // Pass your data
-                    doctype: link.link_doctype, // Doctype name
-                    crud: ['Project','Proposal'].includes(link.link_doctype) ? false : true,      // Enable CRUD operations (optional)
-                    frm: frm,       // Pass the current form object (optional)
-                    options:{
-                        connection: link,
-                        serialNumberColumn: true, // Enable serial number column (optional)
-                        editable: false,      // Enable editing (optional),
-                    }
-                });
-            }
+const tabContent = async (frm, tab_field) => {
+    let field = frm.meta?.fields?.find(f => f.fieldname == tab_field)
+    let _fields = frm.meta?.fields?.filter(f => field?.default?.split(',')?.includes(f.fieldname))
+    for (let _f of _fields) {
+        let link = frm.meta.links?.find(f => f.link_doctype == _f.default)
+        if (link) {
+            let datatable = new SvaDataTable({
+                wrapper: document.querySelector(`#${_f.fieldname}`), // Wrapper element
+                doctype: link.link_doctype, // Doctype name
+                crud: ['Project', 'Proposal'].includes(link.link_doctype) ? false : true,      // Enable CRUD operations (optional)
+                frm: frm,       // Pass the current form object (optional)
+                options: {
+                    connection: link,
+                    serialNumberColumn: true, // Enable serial number column (optional)
+                    editable: false,      // Enable editing (optional),
+                }
+            });
         }
     }
 }
 
 frappe.ui.form.on("Organisation", {
-	refresh(frm) {
+    refresh(frm) {
         let tab_field = frm.get_active_tab()?.df?.fieldname;
         tabContent(frm, tab_field)
         $('a[data-toggle="tab"]').on('shown.bs.tab', async function (e) {
             let tab_field = frm.get_active_tab()?.df?.fieldname;
             tabContent(frm, tab_field)
         });
-	},
+    },
 });
