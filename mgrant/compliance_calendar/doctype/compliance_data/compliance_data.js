db3c51de90041e4a12b214e6e1fd1464a23049d4
Abhishek Yadav <abhi.suvaidyam@gmail.com>
2024-11-01 11:18:49 +0530

    Remove Compliance Calendar

--- mgrant/compliance_calendar/doctype/compliance_data/compliance_data.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2024, Abhishek Yadav and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("Compliance Data", {
-// 	refresh(frm) {
-
-// 	},
-// });
