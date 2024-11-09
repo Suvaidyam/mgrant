c1ce4af0dcd284b0add363fcc16e8c9aeff9cb83
Samar Raj <samar.suvaidyam@gmail.com>
2024-11-09 10:27:34 +0000

    removed unnecessary doctypes and renamed doctypes according to shhet

--- mgrant/mgrant___master/doctype/demography_group/demography_group.js
+++ mgrant/mgrant___master/doctype/demography_group/demography_group.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2024, Suvaidyam and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Entity", {
+// frappe.ui.form.on("Demography Group", {
 // 	refresh(frm) {
 
 // 	},
--- mgrant/mgrant___master/doctype/demography_group_child/demography_group_child.py
+++ mgrant/mgrant___master/doctype/demography_group_child/demography_group_child.py
@@ -5,5 +5,5 @@
 from frappe.model.document import Document
 
 
-class OrganisationBudgetChild(Document):
+class DemographyGroupChild(Document):
 	pass
