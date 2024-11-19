b1704b79b958e7fb27a6755b73132b28a4f44120
Samar Raj <samar.suvaidyam@gmail.com>
2024-11-19 14:24:22 +0000

    changed donor role

--- mgrant/mgrant/doctype/donor/donor.py
+++ mgrant/mgrant/doctype/donor/donor.py
@@ -9,7 +9,7 @@ class Donor(Document):
 	def after_insert(self):
 		new_user = frappe.new_doc("SVA User")
 		new_user.email = self.email
-		new_user.role_profile = "Donor"
+		new_user.role_profile = "System Manager"
 		if len(self.donor_name.split(" ")) > 2:
 			new_user.first_name = self.donor_name.split(" ")[0]
 			new_user.middle_name = self.donor_name.split(" ")[1]
@@ -20,9 +20,3 @@ class Donor(Document):
 		else:
 			new_user.first_name = self.donor_name
 		new_user.insert(ignore_permissions=True,ignore_mandatory=True)
-		if new_user:
-			usr_perm = frappe.new_doc("User Permission")
-			usr_perm.user = new_user.email
-			usr_perm.allow = "Donor"
-			usr_perm.for_value = self.name
-			usr_perm.insert(ignore_permissions=True)
