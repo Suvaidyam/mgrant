41beaf12888ef185fd70d721ef3df6b6734cbd3e
Samar Raj <samar.suvaidyam@gmail.com>
2024-11-25 08:21:38 +0000

    datatable conf fixture

--- mgrant/mgrant___ngo/doctype/proposal/proposal.js
+++ mgrant/mgrant___ngo/doctype/proposal/proposal.js
@@ -21,33 +21,48 @@ const getDocList = (doctype, filters, fields = ['*']) => {
     })
 }
 
+async function set_value(doctype_name, name) {
+    frappe.call({
+        "method": "frappe.client.set_value",
+        "args": {
+            "doctype": doctype_name,
+            "name": name,
+            "fieldname": {
+                "seen": 1
+            },
+        }
+    });
+}
+
+const getViewSettings = (doctype) => {
+    return new Promise((resolve, reject) => {
+        frappe.call({
+            method: "frappe_theme.api.get_my_list_settings",
+            args: { doctype: doctype },
+            callback: function (response) {
+                if (response.message) {
+                    resolve(response.message);
+                } else {
+                    reject(new Error('No message returned from server.'));
+                }
+            },
+            error: (err) => {
+                console.error('Permission error:', err);
+                reject(new Error('You do not have permission to access this resource.'));
+            }
+        });
+    });
+}
+
 const tabContent = async (frm, tab_field) => {
-    let field = frm.meta?.fields?.find(f => f.fieldname == tab_field)
-    let _fields = frm.meta?.fields?.filter(f => field?.default?.split(',')?.includes(f.fieldname))
     if (tab_field === 'gallery_tab') {
         gallery_image(frm);
     }
     if (tab_field === 'email_tab') {
         cominucation(frm);
     }
-    for (let _f of _fields) {
-        let link = frm.meta.links?.find(f => f.link_doctype == _f.default)
-        if (link) {
-            new SvaDataTable({
-                wrapper: document.querySelector(`#${_f.fieldname}`), // Wrapper element   // Pass your data
-                doctype: link.link_doctype, // Doctype name
-                crud: true,      // Enable CRUD operations (optional)
-                frm: frm,       // Pass the current form object (optional)
-                options: {
-                    connection: link,
-                    serialNumberColumn: true, // Enable serial number column (optional)
-                    editable: false,      // Enable editing (optional),
-                }
-            });
-        }
-        if (_f.default === 'tasks') {
-            getTaskList(_f, frm)
-        }
+    if (tab_field === 'tasks_tab') {
+        getTaskList(frm)
     }
 }
 
@@ -59,8 +74,5 @@ frappe.ui.form.on("Proposal", {
             let tab_field = frm.get_active_tab()?.df?.fieldname;
             tabContent(frm, tab_field)
         });
-        // if (!frm.doc.__unsave && frm.doc.stages == "Grant Letter Signed") {
-        //     frm.disable_form();
-        // }
     },
 });
