a3b6541bbdd4f5caf4c8679f040bed5ff86bb5ae
Samar Raj <samar.suvaidyam@gmail.com>
2024-11-29 16:56:38 +0000

    code for commenting background logics of dts

--- mgrant/mgrant/doctype/budget_transaction/budget_transaction.py
+++ mgrant/mgrant/doctype/budget_transaction/budget_transaction.py
@@ -6,24 +6,25 @@ from frappe.model.document import Document
 
 
 class BudgetTransaction(Document):
-	def on_update(self):
-		budget = frappe.get_doc("Budget", self.budget)
-		transactions = frappe.get_all("Budget Transaction", filters={"budget": self.budget}, fields=["transaction"])
-		total_expense = 0
-		for transaction in transactions:
-			total_expense += transaction.transaction if transaction.transaction else 0
-		if budget.allocation < total_expense:
-			frappe.throw("Total expense cannot be greater than allocation")
-		else:
-			budget.expense = total_expense
-		budget.save(ignore_permissions=True)
+    pass
+	# def on_update(self):
+	# 	budget = frappe.get_doc("Budget", self.budget)
+	# 	transactions = frappe.get_all("Budget Transaction", filters={"budget": self.budget}, fields=["transaction"])
+	# 	total_expense = 0
+	# 	for transaction in transactions:
+	# 		total_expense += transaction.transaction if transaction.transaction else 0
+	# 	if budget.allocation < total_expense:
+	# 		frappe.throw("Total expense cannot be greater than allocation")
+	# 	else:
+	# 		budget.expense = total_expense
+	# 	budget.save(ignore_permissions=True)
   
-	def on_trash(self):
-		budget = frappe.get_doc("Budget", self.budget)
-		transactions = frappe.get_all("Budget Transaction", filters={"budget": self.budget}, fields=["transaction","name"])
-		total_expense = 0
-		for transaction in transactions:
-			total_expense += transaction.transaction if (transaction.transaction and transaction.name != self.name) else 0
-		budget.expense = total_expense
-		budget.save(ignore_permissions=True)
+	# def on_trash(self):
+	# 	budget = frappe.get_doc("Budget", self.budget)
+	# 	transactions = frappe.get_all("Budget Transaction", filters={"budget": self.budget}, fields=["transaction","name"])
+	# 	total_expense = 0
+	# 	for transaction in transactions:
+	# 		total_expense += transaction.transaction if (transaction.transaction and transaction.name != self.name) else 0
+	# 	budget.expense = total_expense
+	# 	budget.save(ignore_permissions=True)
 		
--- mgrant/mgrant___ngo/doctype/ngo/ngo.py
+++ mgrant/mgrant___ngo/doctype/ngo/ngo.py
@@ -1,28 +1,29 @@
 # Copyright (c) 2024, Suvaidyam and contributors
 # For license information, please see license.txt
 
-import frappe
+# import frappe
 from frappe.model.document import Document
 
 
 class NGO(Document):
-	def after_insert(self):
-		new_user = frappe.new_doc("SVA User")
-		new_user.email = self.email
-		new_user.role_profile = "NGO"
-		if len(self.ngo_name.split(" ")) > 2:
-			new_user.first_name = self.ngo_name.split(" ")[0]
-			new_user.middle_name = self.ngo_name.split(" ")[1]
-			new_user.last_name = self.ngo_name.split(" ")[2]
-		elif len(self.ngo_name.split(" ")) > 1:
-			new_user.first_name = self.ngo_name.split(" ")[0]
-			new_user.last_name = self.ngo_name.split(" ")[1]
-		else:
-			new_user.first_name = self.ngo_name
-		new_user.insert(ignore_permissions=True,ignore_mandatory=True)
-		if new_user:
-			usr_perm = frappe.new_doc("User Permission")
-			usr_perm.user = new_user.email
-			usr_perm.allow = "NGO"
-			usr_perm.for_value = self.name
-			usr_perm.insert(ignore_permissions=True)
+    pass
+	# def after_insert(self):
+	# 	new_user = frappe.new_doc("SVA User")
+	# 	new_user.email = self.email
+	# 	new_user.role_profile = "NGO"
+	# 	if len(self.ngo_name.split(" ")) > 2:
+	# 		new_user.first_name = self.ngo_name.split(" ")[0]
+	# 		new_user.middle_name = self.ngo_name.split(" ")[1]
+	# 		new_user.last_name = self.ngo_name.split(" ")[2]
+	# 	elif len(self.ngo_name.split(" ")) > 1:
+	# 		new_user.first_name = self.ngo_name.split(" ")[0]
+	# 		new_user.last_name = self.ngo_name.split(" ")[1]
+	# 	else:
+	# 		new_user.first_name = self.ngo_name
+	# 	new_user.insert(ignore_permissions=True,ignore_mandatory=True)
+	# 	if new_user:
+	# 		usr_perm = frappe.new_doc("User Permission")
+	# 		usr_perm.user = new_user.email
+	# 		usr_perm.allow = "NGO"
+	# 		usr_perm.for_value = self.name
+	# 		usr_perm.insert(ignore_permissions=True)
