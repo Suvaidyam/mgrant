7de8195b873b525bcacf81771260d697a32d8628
abhirock74 <abhishek.suvaidyam@gmail.com>
2024-12-02 07:12:50 +0000

    add changes

--- mgrant/mgrant/doctype/budget_transaction/budget_transaction.py
+++ mgrant/mgrant/doctype/budget_transaction/budget_transaction.py
@@ -28,22 +28,54 @@ class BudgetTransaction(Document):
     # 		total_expense += transaction.transaction if (transaction.transaction and transaction.name != self.name) else 0
     # 	budget.expense = total_expense
     # 	budget.save(ignore_permissions=True)frappe
-    def validate(self):
-        date_object = datetime.strptime(self.as_on_date, "%Y-%m-%d")
-        month_name = date_object.strftime("%B")
+    def on_update(self):
+        get_all_transaction = frappe.db.get_list("Budget Transaction", filters={"budget_plan":self.budget_plan}, fields=["transaction","budget_plan","as_on_date"])
         buget_plan_utl_doc = frappe.get_doc("Budget Plan and Utilisation", self.budget_plan)
-        if month_name in ("January", "February", "March"):
-            buget_plan_utl_doc.q4_jan_mar_planned_budget += self.transaction
-        elif month_name in ("April", "May", "June"):
-            buget_plan_utl_doc.q1_apr_jun_utilisation += self.transaction
-        elif month_name in ("July", "August", "September"):
-            buget_plan_utl_doc.q2_jul_sep_utilisation += self.transaction
-        elif month_name in ("October", "November", "December"):
-            buget_plan_utl_doc.q3_oct_dec_utilisation += self.transaction
-        else:
-            print("Invalid month name")
+        q1_uti = 0
+        q2_uti = 0
+        q3_uti = 0
+        q4_uti = 0
+        for trans in get_all_transaction:
+            date_object = datetime.strptime(str(trans.as_on_date), "%Y-%m-%d")
+            month_name = date_object.strftime("%B")
+            print(20 * "k", month_name)
+            if month_name in ("January", "February", "March"):
+                q4_uti += trans.transaction
+            elif month_name in ("April", "May", "June"):
+                q1_uti += trans.transaction
+            elif month_name in ("July", "August", "September"):
+                q2_uti += trans.transaction
+            elif month_name in ("October", "November", "December"):
+                q3_uti += trans.transaction
+        buget_plan_utl_doc.q1_apr_jun_utilisation = q1_uti
+        buget_plan_utl_doc.q2_jul_sep_utilisation = q2_uti
+        buget_plan_utl_doc.q3_oct_dec_utilisation = q3_uti
+        buget_plan_utl_doc.q4_jan_mar_planned_budget = q4_uti
         buget_plan_utl_doc.save()
         
-
+    def on_trash(self):
+        get_all_transaction = frappe.db.get_list("Budget Transaction", filters={"budget_plan":self.budget_plan,'name':['!=',self.name]}, fields=["transaction","budget_plan","as_on_date"])
+        buget_plan_utl_doc = frappe.get_doc("Budget Plan and Utilisation", self.budget_plan)
+        q1_uti = 0
+        q2_uti = 0
+        q3_uti = 0
+        q4_uti = 0
+        for trans in get_all_transaction:
+            date_object = datetime.strptime(str(trans.as_on_date), "%Y-%m-%d")
+            month_name = date_object.strftime("%B")
+            print(20 * "k", month_name)
+            if month_name in ("January", "February", "March"):
+                q4_uti += trans.transaction
+            elif month_name in ("April", "May", "June"):
+                q1_uti += trans.transaction
+            elif month_name in ("July", "August", "September"):
+                q2_uti += trans.transaction
+            elif month_name in ("October", "November", "December"):
+                q3_uti += trans.transaction
+        buget_plan_utl_doc.q1_apr_jun_utilisation = q1_uti
+        buget_plan_utl_doc.q2_jul_sep_utilisation = q2_uti
+        buget_plan_utl_doc.q3_oct_dec_utilisation = q3_uti
+        buget_plan_utl_doc.q4_jan_mar_planned_budget = q4_uti
+        buget_plan_utl_doc.save()
             
         
