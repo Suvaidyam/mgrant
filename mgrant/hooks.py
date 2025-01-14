085f9d145f63a8db9c2aaf4c33027a007fbf992e
Samar Raj <samar.suvaidyam@gmail.com>
2025-01-14 07:45:44 +0000

    code for comment remove

--- mgrant/hooks.py
+++ mgrant/hooks.py
@@ -44,7 +44,6 @@ app_include_js = [
     f"/assets/mgrant/js/task.js?ver={time.time()}",
     f"/assets/mgrant/js/gallery.js?ver={time.time()}",
     f"/assets/mgrant/js/note.js?ver={time.time()}",
-    f"/assets/mgrant/js/comment.js?ver={time.time()}",
     f"/assets/mgrant/js/timeline.js?ver={time.time()}",
     f"/assets/mgrant/js/list_functions.js?ver={time.time()}",
     f"/assets/mgrant/js/kanban.js?ver={time.time()}",
