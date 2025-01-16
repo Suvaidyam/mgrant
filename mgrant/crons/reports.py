909a325b9cba840c86656856e674497c1d9ccdca
Samar Raj <samar.suvaidyam@gmail.com>
2025-01-16 17:16:22 +0000

    code for background jobs

---
+++ mgrant/crons/reports.py
@@ -0,0 +1,19 @@
+import frappe
+@frappe.whitelist()
+def mark_reports_as_delayed():
+    reportings = frappe.db.sql("""
+        SELECT 
+            name
+        FROM 
+            `tabReporting`
+        WHERE
+            due_date < CURDATE()
+            AND submission_status NOT IN ('Submitted','Delayed')
+            AND due_date IS NOT NULL;
+    """,as_dict=True)
+    if len(reportings) > 0:
+        for reporting in reportings:
+            reporting_doc = frappe.get_doc('Reporting',reporting.name)
+            reporting_doc.submission_status = 'Delayed'
+            reporting_doc.flags.ignore_mandatory = True
+            reporting_doc.save(ignore_permissions=True)
---
+++ mgrant/crons/tasks.py
@@ -0,0 +1,19 @@
+import frappe
+@frappe.whitelist()
+def mark_tasks_as_delayed():
+    tasks = frappe.db.sql("""
+        SELECT 
+            name
+        FROM 
+            `tabmGrant Task`
+        WHERE 
+            due_date < CURDATE()
+            AND status NOT IN ('Done','Cancelled','Delayed')
+            AND due_date IS NOT NULL;
+    """,as_dict=True)
+    if len(tasks) > 0:
+        for task in tasks:
+            task_doc = frappe.get_doc('mGrant Task',task.name)
+            task_doc.status = 'Delayed'
+            task_doc.flags.ignore_mandatory = True
+            task_doc.save(ignore_permissions=True)
