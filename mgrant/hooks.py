5db344fd9acde6d5936f352ecf017390fe4a6084
amreshyadavgit <ami92.raj@gmail.com>
2025-02-07 08:22:12 +0000

    exported fixtures

--- mgrant/hooks.py
+++ mgrant/hooks.py
@@ -20,22 +20,22 @@ fixtures = [
     # 'Role',
     # 'Role Profile',
     # "DDFR",
-    'SVADatatable Configuration',
-    'Client Script',
-    'Custom Field',
-    "Property Setter",
-    "Server Script",
-    "Translation",
-    'Custom HTML Block',
+    # 'SVADatatable Configuration',
+    # 'Client Script',
+    # 'Custom Field',
+    # "Property Setter",
+    # "Server Script",
+    # "Translation",
+    # 'Custom HTML Block',
     # "My Theme",
     # "Workflow",
     # "Workflow Action Master",
     # "Workflow State",
     # "RFP Type",
-    "Notification",
-    "Print Format",
-    "Global Search Settings",
-    "Custom DocPerm"
+    # "Notification",
+    # "Print Format",
+    # "Global Search Settings",
+    # "Custom DocPerm"
 
 ]
 # include js, css files in header of desk.html
