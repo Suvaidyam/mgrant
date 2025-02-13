d3b4d8ba321ef0bc33b45cd9c7118149b5c17999
Samar Raj <samar.suvaidyam@gmail.com>
2025-02-13 08:04:03 +0000

    user creation logic

--- mgrant/mgrant/doctype/donor/donor.py
+++ mgrant/mgrant/doctype/donor/donor.py
@@ -3,21 +3,9 @@
 
 import frappe
 from frappe.model.document import Document
+from mgrant.controllers.donor.donor import donor_after_insert
 
 
 class Donor(Document):
-    pass
-	# def after_insert(self):
-	# 	new_user = frappe.new_doc("SVA User")
-	# 	new_user.email = self.email
-	# 	new_user.role_profile = "System Manager"
-	# 	if len(self.donor_name.split(" ")) > 2:
-	# 		new_user.first_name = self.donor_name.split(" ")[0]
-	# 		new_user.middle_name = self.donor_name.split(" ")[1]
-	# 		new_user.last_name = self.donor_name.split(" ")[2]
-	# 	elif len(self.donor_name.split(" ")) > 1:
-	# 		new_user.first_name = self.donor_name.split(" ")[0]
-	# 		new_user.last_name = self.donor_name.split(" ")[1]
-	# 	else:
-	# 		new_user.first_name = self.donor_name
-	# 	new_user.insert(ignore_permissions=True,ignore_mandatory=True)
+	def after_insert(self):
+		donor_after_insert(self)
