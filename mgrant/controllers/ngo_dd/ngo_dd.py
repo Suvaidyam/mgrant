ade7d755f520fc70ee9407bd8221ffdd2fc55c26
Samar Raj <samar.suvaidyam@gmail.com>
2025-02-14 04:14:17 +0000

    code for ngo dd management

--- mgrant/controllers/ngo_dd/ngo_dd.py
+++ mgrant/controllers/ngo_dd/ngo_dd.py
@@ -13,3 +13,11 @@ def ngo_dd_on_update(self):
             ngo_dds = frappe.get_list("NGO Due Diligence", filters={"ngo": self.ngo,'name':['!=',self.name],"status":["!=","Inactive"]},pluck='name',limit=1000,ignore_permissions=True)
             for ngo_dd in ngo_dds:
                 frappe.db.set_value("NGO Due Diligence", ngo_dd, "status", "Inactive",update_modified=False)
+            ngo_doc = frappe.get_doc("NGO", self.ngo)
+            ngo_doc.is_due_diligence_cleared = "Yes"
+            ngo_doc.due_diligence_validation = self.due_diligence_validation
+            ngo_doc.reasons = []
+            ngo_doc.reason_for_due_diligence_fail = ""
+            ngo_doc.save(ignore_permissions=True)
+            
+            
